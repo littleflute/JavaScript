53101c8cfd963d21bb003a55f148d1e4f0224473
littleflute <13699175164@sina.cn>
2018-03-08 13:59:20 -0800

    Update c3.js

--- issues/7/c3.js
+++ issues/7/c3.js
@@ -2,7 +2,7 @@ function api3() {
   function _api3JobClass(){
    var _nTime = 0;
    var _vTimer = null;
-    this.v = "v0.0. 135";  
+    this.v = "v0.0. 143";  
    var _TimeFun = function(){	 				 	
        var p = bl$("id_mp3Player"); 	
       _nTime ++;
@@ -22,17 +22,16 @@ function api3() {
     this.blrMp3List = function(b,d){ 
             if(!d.v){
                  d.v = blo0.blDiv(d,d.id+"v","v",blColor[0]);
-                 function _loadIssue8Comments(o) {
+                 function _loadIssue9Comments(o) {
 				for(i in o){
 					var a = o[i].body;
-					var b = a.split("~~~html");
-					var c = b[1].split("~~~"); 
-					d.v.innerHTML += c[0]; 
+					 
+					d.v.innerHTML +=a; 
 					d.v.innerHTML += "<br>";
 				}
 
 			}
-			w3.getHttpObject("https://api.github.com/repos/littleflute/JavaScript/issues/8/comments", _loadIssue8Comments);	
+			w3.getHttpObject("https://api.github.com/repos/littleflute/JavaScript/issues/9/comments", _loadIssue9Comments);	
              }
 
 		_on_off_div(b,d);		
@@ -54,7 +53,8 @@ this.blrAboutI7C3= function(b,d){
 		_on_off_div(b,d);
 		d.style.background = blGrey[5];
 		b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
-	}
+	}//this.blrAboutI7C3
+
      setInterval(_TimeFun , 100);	
   }//_api3JobClass
 
@@ -65,6 +65,7 @@ if(!d.v){
   blo0.blShowObj2Div(d.v, new _api3JobClass); 
   if(bl$("blrMp3Player")){bl$("blrMp3Player").click();}
   if(bl$("blrMp3List")){bl$("blrMp3List").click();}
+  if(bl$("blrAboutI7C3")){bl$("blrAboutI7C3").click();bl$("blrAboutI7C3").click();}
 }
 _on_off_div(this,d);
 var b = this;
