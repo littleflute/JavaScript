07f660b995d1488eadb85a0f4fb61bc71a365e82
littleflute <13699175164@sina.cn>
2018-03-11 22:15:58 -0700

    Create i.js

---
+++ issues/9/i.js
@@ -0,0 +1,26 @@
+var _parseFun = function (d,txt){
+      var str = "var a =" +  txt;  
+     eval(str);
+     d.innerHTML = "";
+     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 <a/> - ";
+      s += "<a href='https://github.com/littleflute/JavaScript/edit/master/issues/9/i.js' target='_blank'>i.js - <a/>";
+      s += "<a href='https://littleflute.github.io/JavaScript/issues/9/i.js*' target='_blank'>i.js - <a/>";
+     blo0.blDiv(d,d.id+"_#9_", s,blGrey[0]);
+     blo0.blDiv(d,d.id+"_title_", a.title,blGrey[0]);
+
+     for(i in a.songs){
+           var dSong = blo0.blDiv(d,d.id+"_mp3_"+i, a.songs[i].mp3,blColor[i]);
+           dSong .onclick = function(_this){ 
+                return function(){ 
+                   _play (_this);
+                }
+           }(dSong )
+           dSong .onmouseover= function(_this){
+                
+                return function(){ _this.style.background = "blue";}
+           }(dSong )
+           dSong .onmouseout= function(_this){ 
+                return function(){ _this.style.background = "grey";}
+           }(dSong )
+      }
+  }
