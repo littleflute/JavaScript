ec63d59f10ff5907862596155ceb472b8bf63864
littleflute <13699175164@sina.cn>
2018-03-11 22:45:50 -0700

    Update i.js

--- issues/9/i.js
+++ issues/9/i.js
@@ -2,15 +2,15 @@ var _parseFun = function (d,txt){
       var str = "var a =" +  txt;  
      eval(str);
      d.innerHTML = "";
-     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 v0.0. 22<a/> - "; 
+     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 v0.0. 24<a/> - "; 
     s += "<a href='https://github.com/littleflute/JavaScript/edit/master/issues/9/i.js' target='_blank'>i.js* <a/> - ";
     s += "<a href='https://littleflute.github.io/JavaScript/issues/9/i.js' target='_blank'>i.js<a/>";
-     blo0.blDiv(d,d.id+"_#9_", s,blGrey[0]);
-     blo0.blDiv(d,d.id+"_title_", a.title,blGrey[0]);
+     blo0.blDiv(d,d.id+"_#9_", s,blColor[4]);
+     blo0.blDiv(d,d.id+"_title_", a.title,blColor[4]);
  
      for(i in a.songs){
            var p = bl$("id_mp3Player");
-           var dSong = blo0.blDiv(d,d.id+"_mp3_"+i, a.songs[i].mp3,blColor[i]);
+           var dSong = blo0.blDiv(d,d.id+"_mp3_"+i, a.songs[i].mp3,blGrey[5]);
            dSong .onclick = function(_this){ 
                 return function(){ 
                    if(!p.srcNow) p.srcNow = "";
