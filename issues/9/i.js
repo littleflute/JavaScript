aeb90daab105df674691ecf4e24ca73e13ddcce8
littleflute <13699175164@sina.cn>
2018-03-11 22:25:57 -0700

    Update i.js

--- issues/9/i.js
+++ issues/9/i.js
@@ -2,8 +2,9 @@ var _parseFun = function (d,txt){
       var str = "var a =" +  txt;  
      eval(str);
      d.innerHTML = "";
-     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 v0.0.5<a/> "; 
-    s += "<a href='https://github.com/littleflute/JavaScript/edit/master/issues/9/i.js' target='_blank'>i.js* <a/>";
+     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 v0.0.11<a/> - "; 
+    s += "<a href='https://github.com/littleflute/JavaScript/edit/master/issues/9/i.js' target='_blank'>i.js* <a/> - ";
+    s += "<a href='https://littleflute.github.io/JavaScript/issues/9/i.js' target='_blank'>i.js<a/>";
      blo0.blDiv(d,d.id+"_#9_", s,blGrey[0]);
      blo0.blDiv(d,d.id+"_title_", a.title,blGrey[0]);
 
