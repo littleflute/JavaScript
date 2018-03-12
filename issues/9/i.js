c5f437b59f5dc09298d30333144d6e8723553f7e
littleflute <13699175164@sina.cn>
2018-03-11 22:53:26 -0700

    Update i.js

--- issues/9/i.js
+++ issues/9/i.js
@@ -2,7 +2,7 @@ var _parseFun = function (d,txt){
       var str = "var a =" +  txt;  
      eval(str);
      d.innerHTML = "";
-     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 v0.0. 24<a/> - "; 
+     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 v0.0. 25<a/> - "; 
     s += "<a href='https://github.com/littleflute/JavaScript/edit/master/issues/9/i.js' target='_blank'>i.js* <a/> - ";
     s += "<a href='https://littleflute.github.io/JavaScript/issues/9/i.js' target='_blank'>i.js<a/>";
      blo0.blDiv(d,d.id+"_#9_", s,blColor[4]);
@@ -13,9 +13,9 @@ var _parseFun = function (d,txt){
            var dSong = blo0.blDiv(d,d.id+"_mp3_"+i, a.songs[i].mp3,blGrey[5]);
            dSong .onclick = function(_this){ 
                 return function(){ 
-                   if(!p.srcNow) p.srcNow = "";
-                   if(_this.innerHTML != p.srcNow){
-                     p.srcNow = p.src = _this.innerHTML;
+                   if(!p.dNow) p.dNow= null;
+                   if(_this!= p.dNow){
+                     p.src = _this.innerHTML; p.dNow = _this;
                      p.play();
                      _this.style.background = blGrey[0];
                    }
@@ -23,16 +23,16 @@ var _parseFun = function (d,txt){
            }(dSong )
            dSong .onmouseover= function(_this){                
                 return function(){
-                   if(!p.srcNow) p.srcNow = "";
-                   if(_this.innerHTML != p.srcNow){ 
+                   if(!p.dNow) p.dNow= null;
+                   if(_this!= p.dNow){ 
                      _this.style.background = blGrey[3];
                    }
                  }
            }(dSong )
            dSong .onmouseout= function(_this){ 
                 return function(){
-                   if(!p.srcNow) p.srcNow = "";
-                   if(_this.innerHTML != p.srcNow){ 
+                   if(!p.dNow) p.dNow= null;
+                   if(_this != p.dNow){ 
                      _this.style.background = blGrey[5];
                    }
                  }
