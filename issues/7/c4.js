095309ed49e2fe24c65639ef8518770f7df9d18a
littleflute <13699175164@sina.cn>
2018-03-13 09:00:39 -0700

    Update c4.js

--- issues/7/c4.js
+++ issues/7/c4.js
@@ -1,6 +1,13 @@
-function _blVPlayer () {
-    function _vplayerClass () {
-        this.v = "v0.0. 42";
+function _blVPlayer ( _p ) {//api4
+    function _vplayerClass ( _o) {
+        this.v = "v0.0. 44";
+        this.blrList= function(b,d){
+            if(!d.v){
+                 d.v = blo0.blDiv(d,d.id+"v","v" ,blGrey[1]); 
+                 blo0.blShowObj2Div(d.v, _o);
+                 ;
+            }
+        }
         this.blrAboutI7C4= function(b,d){		var s = "blrAboutI7C4<br>"; 
 		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/issues/7'"
 		s += " style='color:blue;'"; 		s +=">"; 		s += "#7*  "; 		s += "</a>"; 
@@ -15,8 +22,7 @@ function _blVPlayer () {
 var v = document.body;
 var d = blo0.blMDiv(v,v.id+"_blVPlayer ","_blVPlayer",151,10,500,400,blGrey[0]);
 d.v = blo0.blDiv(d,d.id+"v","v" ,blGrey[1]);
-
-blo0.blShowObj2Div(d.v, new _vplayerClass);
+blo0.blShowObj2Div(d.v, new _vplayerClass ( _p ) );
 var b = bl$("blrToolBarDivtbbtnLoadb3");
  
 _on_off_div(b,d); 
