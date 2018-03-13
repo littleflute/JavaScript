23608102f0d7f37e579fe5d528d84adaccd2ff5c
littleflute <13699175164@sina.cn>
2018-03-13 11:06:20 -0700

    Create c5.js

---
+++ issues/7/c5.js
@@ -0,0 +1,33 @@
+function _blSet ( _p ) {//api5
+    function _vplayerClass ( _o) {
+        this.v = "v0.0. 53";
+        this.blrSetList= function(b,d){
+            if(!d.v){
+                 d.v = blo0.blDiv(d,d.id+"v","v" ,blGrey[1]); 
+                 blo0.blShowObj2Div(d.v, _o);
+                 ;
+            }
+           _on_off_div(b,d); 		d.style.background = blGrey[5]; 		b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
+        }
+        this.blrAboutI7C5= function(b,d){		var s = "blrAboutI7C5<br>"; 
+		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/issues/7'"
+		s += " style='color:blue;'"; 		s +=">"; 		s += "#7*  "; 		s += "</a>"; 
+		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/edit/master/issues/7/c5.js'";
+		s += " style='color:blue;'"; 		s +=">"; 		s += " c5.js*  ";		s += "</a>"; 
+		s += "<a target='_blank'  href='https://littleflute.github.io/JavaScript/issues/7/c5.js'";
+		s += " style='color:yellow;'"; 		s +=">";		s += " c5.js ";		s += "</a>"; 
+		d.innerHTML = s;
+		_on_off_div(b,d); 		d.style.background = blGrey[5]; 		b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
+	}//this.blrAboutI7C5
+    }
+var v = document.body;
+var d = blo0.blMDiv(v,v.id+"_blSet","_blSet",351,10,500,400,blGrey[0]);
+ 
+d.v = blo0.blDiv(d,d.id+"v","v" ,blGrey[1]);
+blo0.blShowObj2Div(d.v, new _vplayerClass ( _p ) ); 
+if(bl$("blrAboutI7C5")){bl$("blrAboutI7C5").click();}
+
+var b = bl$("blrToolBarDivtbbtnLoadb4");_on_off_div(b,d); 
+b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
+b.innerHTML = "[blSet]";
+}
