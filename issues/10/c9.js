2c412c10454f0c1ec9549f8eddda476b6413a893
littleflute <13699175164@sina.cn>
2018-03-13 23:28:42 -0700

    Update c9.js

--- issues/10/c9.js
+++ issues/10/c9.js
@@ -1,18 +1,13 @@
 var p = bl$("id_div_4_api2_v"); p.title = "";
 if(!p.v1){ 
-     var b = blo0.blBtn(p, p.id + "_btn_4_v1", "v1_v0.0. 13", blColor[1]);
-     var s = "#10-C3<br>"; 
-		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/issues/10'"
-		s += " style='color:blue;'";
-		s +=">";
-		s += "#10*  ";
-		s += "</a>"; 
+     var b = blo0.blBtn(p, p.id + "_btn_4_v1", "v1_v0.0. 14", blColor[1]);
+     var s = "#10-C3:";  
 		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/edit/master/issues/10/c9.js'"
 		s += " style='color:blue;'";		s +=">";		s += " c9.js*  ";		s += "</a>"; 
 		s += "<a target='_blank'  href='https://littleflute.github.io/JavaScript/issues/10/c9.js'";
 		s += " style='color:yellow;'";		s +=">";		s += " c9.js ";		s += "</a>"; 
 
-     p.v1 = blo0.blDiv(p,p.id+"v1",s,"blue");
+     p.v1 = blo0.blDiv(p,p.id+"v1",s, blColor[4]);
      p.v1.bs = []; 
      b.onclick = function(_this){ return function(){
           _on_off_div(_this,p.v1);
