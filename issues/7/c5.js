e05c274d1ea88cbee739abbcb604cdf354772aee
littleflute <13699175164@sina.cn>
2018-03-13 21:39:22 -0700

    Update c5.js

--- issues/7/c5.js
+++ issues/7/c5.js
@@ -1,7 +1,7 @@
 function _blSet ( _p ) {//api5
     if(!_p){_p ={"v":"v0.0.1"};}
     function _setClass ( _o) {
-        this.v = "v0.0. 114";
+        this.v = "v0.0. 115";
         this.blrSetList= function(b,d){
             if(!d.v){
                  d.v = blo0.blDiv(d,d.id+"v","v" ,blGrey[1]); 
@@ -10,9 +10,7 @@ function _blSet ( _p ) {//api5
             }
            _on_off_div(b,d); 		d.style.background = blGrey[5]; 		b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
         }
-        this.blrAboutI7C5= function(b,d){		var s = "blrAboutI7C5<br>"; 
-		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/issues/7'"
-		s += " style='color:blue;'"; 		s +=">"; 		s += "#7*  "; 		s += "</a>"; 
+        this.blrAboutI7C5= function(b,d){		var s = "blrAboutI7C5<br>";  
 		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/edit/master/issues/7/c5.js'";
 		s += " style='color:blue;'"; 		s +=">"; 		s += " c5.js*  ";		s += "</a>"; 
 		s += "<a target='_blank'  href='https://littleflute.github.io/JavaScript/issues/7/c5.js'";
