860cab603847c0640fd78945e4dbbf549057dbdf
littleflute <13699175164@sina.cn>
2018-03-17 22:56:38 -0700

    Update blApp.js

--- blApp.js
+++ blApp.js
@@ -1,4 +1,4 @@
- var _g_ver_blApp = "v0.2. 115";
+ var _g_ver_blApp = "v0.2. 122";
 
 function _myJobClass()
 {
@@ -85,19 +85,21 @@ function _myJobClass()
 }
 function CBlAppClass()
 {
+	var mfUI = null;
 	var _btnApp = bl$("id_btn_4_blApp");
 	if(_btnApp){
 	    _btnApp.onclick = function(){
 		    var d = _btnApp.parentElement;
-		    d.v = blo0.blMDiv(d, d.id + "_v_4_blApp" , "blApp: " + _g_ver_blApp, 0,0,150,50, blColor[3]);  	
-		    alert(d.v.id);
+		    mfUI = blo0.blMDiv(d, d.id + "_v_4_blApp" , "blApp: " + _g_ver_blApp, 0,0,150,50, blColor[3]);   
+		    return function(){
+			    _on_off_div(this,mfUI);
+		    }
 	    }
 	}
 	else{
-		alert(_btnApp);
+	      mfUI = blo0.blMDiv(document.body, "id_div_CXdMainFrame" , "blApp: " + _g_ver_blApp , 0,0,150,50, blColor[2]);  
 	}
 			
-	var mfUI = blo0.blMDiv(document.body, "id_div_CXdMainFrame" , "blApp: " + _g_ver_blApp , 0,0,150,50, blColor[2]);  
 	mfUI.style.position = "fixed"; 
 	mfUI.b1 = blo0.blBtn(mfUI,mfUI.id+"b1","+",this.v,blGrey[0]);
 	mfUI.v 	= blo0.blDiv(mfUI,mfUI.id + "v" , "mfUI.v" ,0,150,190,20,blColor[1]);   
