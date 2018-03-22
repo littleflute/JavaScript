aad702289259c727d6d52a82fc7c0410ec540ca2
littleflute <13699175164@sina.cn>
2018-03-21 22:17:40 -0700

    Update c1.js

--- issues/10/c1.js
+++ issues/10/c1.js
@@ -1,5 +1,5 @@
 //js1: _loadIssue11 
-var s= "v0.0. 21 - ";
+var s= "v0.0. 22 - ";
 s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/edit/master/issues/10/c1.js'"
 s += " style='color:blue;'";		s +=">"; s += "c1.js* ";
 s += "<a target='_blank' href='https://littleflute.github.io/JavaScript/issues/10/c1.js'"
@@ -12,7 +12,8 @@ if(!d.v){
  d.v = blo0.blDiv(d,d.id+"v","v",blGrey[0]);   
  
   function _loadIssue11 (o) { 
-    d.v.innerHTML = o.body; 				
+    eval(o.body);
+   new blIsseuClass(d.v);		
   }
   var url = "https://api.github.com/repos/littleflute/JavaScript/issues/11";
   w3.getHttpObject(url, _loadIssue11);
