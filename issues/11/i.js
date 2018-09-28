3fbc6f98d1fe7716a707bf73473725380716c30b
littleflute <13699175164@sina.cn>
2018-09-27 19:36:46 -0700

    Update i.js

--- issues/11/i.js
+++ issues/11/i.js
@@ -1,8 +1,29 @@
 //
 function blIsseuClass(_d){
    var _toolBar = blo0.blDiv (_d,  _d.id + "_toolBar_4_issue11", "_toolBar_4_issue11", blGrey[1]);
+  function _loadIssue11Comments(o) {
+				var _i = 0;
+				var _s = "<a target='_balnk' href ='";
+				_s += "https://github.com/littleflute/JavaScript/issues/11'";
+				_s += ">#11</a>"; 
+				var _v = _toolBar ;
+				for(i in o){
+					_i++;
+					var a = o[i].body;
+					var btnJS = blo0.blBtn(_v, _v.id+"btnJS"+i,_i,blGrey[2]);
+					btnJS.onclick = function(_txt){
+				               return function(){
+                                                              eval( _txt);
+                                               }
+				        }(a);
+				}
+
+			}
+			var _src = "https://api.github.com/repos/littleflute/JavaScript/issues/11/comments";
+			w3.getHttpObject(_src, _loadIssue11Comments);	
+
    _d.view = blo0.blDiv (_d,  _d.id + "view", "view ", blGrey[2]);
-    this.v = "v0.0. 34- ";
+    this.v = "v0.0. 43- ";
     this.blr_src_blIssueClass = function(b,d){	
         if(!d.v){
              d.v = blo0.blDiv(d, d.id + "v", "", blGrey[0]);
@@ -36,4 +57,7 @@ function blIsseuClass(_d){
 	}//this.blr_about_blIssueClass 
 
     blo0.blShowObj2Div(_d.view ,this); 
+
+
+ 
 }
