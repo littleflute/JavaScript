9daea6ba4a88e518b5338e57093d1d6c7421691f
littleflute <13699175164@sina.cn>
2018-10-02 01:35:02 -0700

    Update i.js

--- issues/9/i.js
+++ issues/9/i.js
@@ -2,7 +2,7 @@ var _parseFun = function (d,txt){
       var str = "var a =" +  txt;  
      eval(str);
      d.innerHTML = "";
-     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 v0.0. 114<a/> - "; 
+     var s = "<a href='https://github.com/littleflute/JavaScript/issues/9' target='_blank'>#9 v0.0. 115<a/> - "; 
     s += "<a href='https://github.com/littleflute/JavaScript/edit/master/issues/9/i.js' target='_blank'>i.js* <a/> - ";
     s += "<a href='https://littleflute.github.io/JavaScript/issues/9/i.js' target='_blank'>i.js<a/>";
      blo0.blDiv(d,d.id+"_#9_", s,blColor[4]);
@@ -43,7 +43,7 @@ var _parseFun = function (d,txt){
                 return function(){
                    if(!p.dNow) p.dNow= null;
                    if(_this != p.dNow){ 
-                     _this.style.background = blGrey[_this.id];
+                     _this.style.background = "green";//blGrey[_this.id];
                    }
                  }
            }(dSong )
