6d6eb49827057ac12d6f5831e591fedffac6ed00
littleflute <13699175164@sina.cn>
2018-10-25 19:49:35 -0700

    Create i.js

---
+++ issues/16/i.js
@@ -0,0 +1,17 @@
+var v = bl$("id_div_4_Plx");
+var _btn = bl$("id_btn_Load_Plx");
+
+var d1 = blo0.blDiv(v, v.id + "_d1",  "d1", blGrey[0]); 
+var s = '<div class="XDheader"><div class="XDheader-right" id="XDid_div_title">';
+ s += '<a href= "https://github.com/littleflute/JavaScript/issues/16" target="_blank">#16_v0.0. 22</a> ';
+s += "<a class='xd' target='_blank' href='https://github.com/littleflute/JavaScript/edit/master/Issues/16/i.js'"
+s +=">";		s += " i16.js*  ";		s += "</a> "; 
+		s += "  <a class='xd'    target='_blank'  href='https://littleflute.github.io/english/Issues/16/i.js'";
+ 
+		s +=">";
+		s += " i16.js ";
+		s += "</a>"; 
+ s += '</div></div>';
+d1.innerHTML = s;
+
+_on_off_div(_btn , v);	
