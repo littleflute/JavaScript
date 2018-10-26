639c3dc218b4b1801d70808d89b8ecb58da67feb
littleflute <13699175164@sina.cn>
2018-10-25 20:43:08 -0700

    Update i.js

--- issues/16/i.js
+++ issues/16/i.js
@@ -3,7 +3,7 @@ var _btn = bl$("id_btn_Load_Plx");
 
 var d1 = blo0.blDiv(v, v.id + "_d1",  "d1", blGrey[0]); 
 var s = '<div class="XDheader"><div class="XDheader-right" id="XDid_div_title">';
- s += '<a href= "https://github.com/littleflute/JavaScript/issues/16" target="_blank">#16_v0.0. 33</a> ';
+ s += '<a href= "https://github.com/littleflute/JavaScript/issues/16" target="_blank">#16_v0.0. 34</a> ';
 s += "<a class='xd' target='_blank' href='https://github.com/littleflute/JavaScript/edit/master/issues/16/i.js'"
 s +=">";		s += " i16.js*  ";		s += "</a> "; 
 	s += "  <a class='xd'  target='_blank' href='https://littleflute.github.io/JavaScript/issues/16/i.js'";
@@ -31,7 +31,7 @@ _btn .plxFn = function(ctx){
         d.v = blo0.blDiv(d,d.id+"v","v",blGrey[2]);
         d.v1 = blo0.blDiv(d,d.id+"v1","v1",blGrey[3]);
 	 for(i in o){ //o[i].body
-                    var _b = blo0.blBtn(d.v , d.v.id + "_btn_" + i, i, blColor[i]);    _b.v = d.v1;
+                    var _b = blo0.blBtn(d.v , d.v.id + "_btn_" + i, i, blGrey[i]);    _b.v = d.v1;
                     _b.onclick = function(_this, _txt){
                          return function(){ _this.v.innerHTML = _txt; eval(_txt);}
                      }(_b, o[i].body);  
