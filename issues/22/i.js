1fd63539ece04cade7574622c290a6111214837a
littleflute <13699175164@sina.cn>
2019-01-15 10:56:50 -0800

    Create i.js

---
+++ issues/22/i.js
@@ -0,0 +1,11 @@
+//ref: https://littleflute.github.io/blog/docs/2017/04/15/ 
+var myV = bl$("id_div_4_i22");
+var s = "";  
+		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/issues/22'"
+		s += " style='color: brown;'";		s +=">";		s += " #22*  ";		s += "</a>"; 
+		s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/edit/master/issues/22/i.js'"
+		s += " style='color:blue;'";		s +=">";		s += " i.js*  ";		s += "</a>"; 
+		s += "<a target='_blank'  href='https://littleflute.github.io/JavaScript/issues/22/i.js'";
+		s += " style='color: yellow;'";		s +=">";		s += " i.js ";		s += "</a>"; 
+		myV .innerHTML = s;
+ 
