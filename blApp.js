fda87f9948c032002f2330324ef719b4a180359f
littleflute <13699175164@sina.cn>
2019-05-23 01:14:42 -0700

    blApp.js ep.bat t.html

--- blApp.js
+++ blApp.js
@@ -1,6 +1,13 @@
- var _g_ver_blApp = "v0.2. 213";
+var s = "<div id = 'id_div_4_blApp' >";
+s += "v0.3. 2 ";
+s += "</div>";
+
+var _g_ver_blApp = s;
  
 function _loadIssue6(o) {	
 	eval(o.body); 
 }
 w3.getHttpObject("https://api.github.com/repos/littleflute/JavaScript/issues/6", _loadIssue6);	
+
+
+ 
