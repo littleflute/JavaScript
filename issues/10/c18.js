922277a24fe307e1fee342fd1700eab1317ec815
littleflute <13699175164@sina.cn>
2019-07-08 16:06:05 -0700

    Create c18.js

---
+++ issues/10/c18.js
@@ -0,0 +1,24 @@
+//c18
+var md = blo0.blDiv(document.body, "idMD","idMD",blGrey[0]);  
+if(!md.run){
+        md.run = true; 
+	var style ="position: absolute;";
+	style += "z-index: 9;";
+	style += "background-color: #f1f1f1;";
+	style += "text-align: center;";
+	style += "border: 1px solid #d3d3d3;";
+	style += "left: 400px";
+	style += "top: 40px";
+	md .style =style;
+
+	var title = blo0.blDiv(md ,"moveDivHeader","header");
+	style ="padding: 10px;";
+	style += "z-index: 10;";
+	style += "cursor: move;";
+	style += "text-align: center;";
+	style += "border: 1px solid #fff;";
+	style += "background-color: #2196F3;";
+	title.style =style;
+        blo0.blMakeDivMovable(md );
+}
+_on_off_div(this,md);
