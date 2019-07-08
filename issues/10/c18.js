2613e9214d9ff65b7a5312ac1a9018ba07ded400
littleflute <13699175164@sina.cn>
2019-07-08 16:12:03 -0700

    Update c18.js

--- issues/10/c18.js
+++ issues/10/c18.js
@@ -1,5 +1,11 @@
 //c18
-var md = blo0.blDiv(document.body, "idMD","idMD",blGrey[0]);  
+var s= "v0.0. 24 ";
+s += "<a target='_blank' href='https://github.com/littleflute/JavaScript/edit/master/issues/10/c18.js'"
+s += " style='color:blue;'";		s +=">"; s += "c18.js* ";
+s += "<a target='_blank' href='https://littleflute.github.io/JavaScript/issues/10/c18.js'"
+s += " style='color:green;'";		s +=">"; s += "c18.js ";
+
+var md = blo0.blDiv(document.body, "idMD", s ,blGrey[0]);  
 if(!md.run){
         md.run = true; 
 	var style ="position: absolute;";
