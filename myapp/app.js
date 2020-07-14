7a43e201b55b32ff47c0ab257c8031894fd6d445
yongling <yongling.huang@group6.io>
2020-07-14 02:25:28 +0000

    app.js v0.12

--- myapp/app.js
+++ myapp/app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
-const tag ="[app.js_v0.11]";
+const tag ="[app.js_v0.12]";
 
-app.get('/', (req, res) => res.send( tag + 'Hello World!'))
-
+app.get('/', (req, res) => res.send( tag + 'Hello World!'+Date() ));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
