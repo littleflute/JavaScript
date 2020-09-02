d5648fade20a25f2dca8bdcfdff8e80f6a40d001
unknown <yongling.huang@group6.io>
2020-09-01 21:12:52 -0700

    i34 *

--- i/34/app.js
+++ i/34/app.js
@@ -1,32 +1,43 @@
-const tag = "[app.js_v0.11]";
-var createError = require('http-errors');
+const tag = "[app.js_v0.14]";
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
+var user = require('./routes/user');
+var auth = require('./routes/auth');
 
+const passport    = require('passport');
+
+require('./passport');
+
+console.log(tag + " xdTest:" + Date());
 var app = express();
 
-console.log(tag+ " xdTest:" + Date());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/user', passport.authenticate('jwt', {session: false}), user);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
@@ -39,5 +50,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
