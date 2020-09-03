114601ce30abdece331d975e9d1ee13e3aeb463d
unknown <yongling.huang@group6.io>
2020-09-03 00:18:03 -0700

    i46

--- i/46/index.js
+++ i/46/index.js
@@ -1,12 +1,14 @@
+const tag = "[index.js_v0.12]";
 var express = require("express");
 var app = express();
 var http = require("http").createServer(app);
 var socketIO = require("socket.io")(http);
  
 http.listen(3000, function () {
-    socketIO.on("connection", function (socket) {
-        socket.on("new_user", function () {
-            socket.broadcast.emit("new_user");
+    socketIO.on("connection", function (_socket) {
+        _socket.on("new_user", function () {
+            console.log(tag+" "+Date());
+            _socket.broadcast.emit("new_user");
         });
     });
 });
