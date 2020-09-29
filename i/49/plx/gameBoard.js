991c8987013abedb7cff30149a600cf01adc9730
unknown <yongling.huang@group6.io>
2020-09-28 20:27:32 -0700

    plx/gameBoard.js*

--- i/49/plx/gameBoard.js
+++ i/49/plx/gameBoard.js
@@ -1,4 +1,4 @@
-const tag = "plx/gameBoard.js_v0.12";
+const tag = "plx/gameBoard.js_v0.13";
 
 function CGameBoard (_btn,_wso){    
     
@@ -12,10 +12,13 @@ function CGameBoard (_btn,_wso){
     var _f = function(){
         var d = blo0.blMD("id_md_4_gameBoard",tag,333,151,444,400,blGrey[0]);
         d.tb = blo0.blDiv(d,d.id+"tb","tb",blGrey[1]);
-        d.tb.b1 = blo0.dbgBtn(d.tb,"test","grey","green",
+        d.tb.b1 = blo0.dbgBtn(d.tb,"dbg","grey","green",
             function(c,_x,_y,_w,_h){
                 blo0.blRect (c,_x,_y,_w,_h,"lightblue");
                 blo0.blText(c,"test.dbg",_x,_y,20,"red");            
+            },
+            function(_btn){//init	  
+                ;
             },
             function(_btn,_x,_y){ 
                 _btn.setDown(true);
