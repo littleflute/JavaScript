bb8e374775bedf7bda781b5f039577a339e72b2e
littleflute <littleflute@163.com>
2020-12-07 20:19:39 -0800

    /voanews

--- i/49/index.js
+++ i/49/index.js
@@ -1,4 +1,4 @@
-const tag = "[i/49/index.js_v0.33]";
+const tag = "[i/49/index.js_v0.34]";
 const u = require("./util.js");
 const app = require("express")();
 var PORT = 3000;
@@ -8,6 +8,9 @@ app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"));
 app.get("/plx/gameBoard.js", (req,res)=> res.sendFile(__dirname + "/plx/gameBoard.js"));
 app.get("/plx/p1.js", (req,res)=> res.sendFile(__dirname + "/plx/p1.js"));
 
+app.get("/voanews", (req,res)=> {
+    res.end("voanews ...");
+};
 
 app.listen(PORT, ()=>console.log(tag + " Listening on http port " + PORT));
 
